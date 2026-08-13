d16774557428f5d329c0c304e477d6a4c6ff11f5
kaibryant1 <kaibryant42@gmail.com>
2026-08-13 12:21:11 +0100

    Adding Screen and Clock

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0 
+
+        
+if __name__ == "__main__":
+    main()
